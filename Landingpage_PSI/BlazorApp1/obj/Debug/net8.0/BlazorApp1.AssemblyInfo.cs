1a36d9a466844eb06dabf6e4cc930da16606c456
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-02-12 15:33:25 -0300

    add

--- Landingpage_PSI/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ Landingpage_PSI/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0016373a507c06fcc3b987ac9e9a26a196510cc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8096736f2de4d56bd3332399f01809cac71b78a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
