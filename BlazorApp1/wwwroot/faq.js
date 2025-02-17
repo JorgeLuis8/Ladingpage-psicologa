498a5eb359b8df7925aa8e36e11852e2d6093413
Jorge Luis <jorge.luz@ufpi.edu.br>
2025-02-16 21:34:52 -0300

    atualizaccao dos botoes e algumas animacoes

---
+++ BlazorApp1/wwwroot/faq.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const faqItems = document.querySelectorAll('.faq-item');
+    
+    faqItems.forEach(item => {
+      item.addEventListener('click', function() {
+        const answer = item.querySelector('.faq-answer');
+        const icon = item.querySelector('.faq-static-icon');
+        
+        // Se o item já estiver aberto, fecha-o
+        if(answer.classList.contains('open')) {
+          answer.classList.remove('open');
+          answer.style.maxHeight = null;
+          icon.textContent = "▶";
+        } else {
+          // Abre o item ajustando o max-height para o tamanho do conteúdo
+          answer.classList.add('open');
+          answer.style.maxHeight = answer.scrollHeight + "px";
+          icon.textContent = "▼";
+        }
+      });
+    });
+  });
